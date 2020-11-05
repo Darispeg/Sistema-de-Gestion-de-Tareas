879be6d20ecd5f82b15b6a9e567f2b6d4dcd70ca
Darispeg <darispegdennis@gmail.com>
2020-11-05 09:39:25 -0400

    actualizando pom.xml

---
+++ application/src/main/java/com/school/application/controller.java
@@ -0,0 +1,12 @@
+package com.school.application;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class controller {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hola como estas?";
+    }
+}
